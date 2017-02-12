5257c4c393cdb8cf4c5cee66abcc392d869bebf7
Ye Lin Aung <me@yelinaung.com>
2017-02-12 11:24:28 +0800

    Should do ln instead of ls =.=

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ ln -s $PWD/vimrc      $HOME/.vimrc
 ln -s $PWD/tmux.conf  $HOME/.tmux.conf
 ln -s $PWD/irbc       $HOME/.irbc
 ln -s $PWD/gitconfig  $HOME/.gitconfig
-ls -s $PWD/vim        $HOME/.config/nvim # neovim
-ls -s $PWD/vimrc      $HOME/.config/nvim/init.vim # neovim
+ln -s $PWD/vim        $HOME/.config/nvim # neovim
+ln -s $PWD/vimrc      $HOME/.config/nvim/init.vim # neovim
 
 echo "Done linking up necessary files."
