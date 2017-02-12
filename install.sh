a5c63259f10bca0bc3d0f4025f7be6dd350d0e03
Ye Lin Aung <me@yelinaung.com>
2017-02-12 03:19:27 +0000

    update for neovim

--- install.sh
+++ install.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
-ln -s $PWD/vim $HOME/.vim
-ln -s $PWD/vimrc $HOME/.vimrc
-ln -s $PWD/tmux.conf $HOME/.tmux.conf
-ln -s $PWD/irbc $HOME/.irbc
-ln -s $PWD/gitconfig $HOME/.gitconfig
+ln -s $PWD/vim        $HOME/.vim
+ln -s $PWD/vimrc      $HOME/.vimrc
+ln -s $PWD/tmux.conf  $HOME/.tmux.conf
+ln -s $PWD/irbc       $HOME/.irbc
+ln -s $PWD/gitconfig  $HOME/.gitconfig
+ls -s $PWD/vim        $HOME/.config/nvim # neovim
+ls -s $PWD/vimrc      $HOME/.config/nvim/init.vim # neovim
 
 echo "Done linking up necessary files."
